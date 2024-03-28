d7dd62847e61e00c906d86fb6f78d21ece022a88
Marinus <mwwretra@st.noorderpoort.nl>
2024-03-28 14:45:45 +0100

    Player can pick up one item

--- Fishing_Game/Assets/Scripts/interactions/PickupInteractable.cs
+++ Fishing_Game/Assets/Scripts/interactions/PickupInteractable.cs
@@ -1,19 +1,22 @@
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 public class PickupInteractable : MonoBehaviour, IInteractable
 {
     public InventoryItemData itemData;
-    public InventoryHolder  playerInventoryHolder;
+    public InventoryHolder playerInventoryHolder;
     void Start()
-    { 
-      //  playerInventoryHolder = GameObject.Find("Player").GetComponent<InventoryHolder>();
+    {
+        //  playerInventoryHolder = GameObject.Find("Player").GetComponent<InventoryHolder>();
+
     }
 
     public void Interact()
     {
         if (playerInventoryHolder.InventorySystem.AddToInventory(itemData, 1))
         {
+            Debug.Log($"{playerInventoryHolder.InventorySystem.InventorySlots.ElementAt(0).ItemData.DisplayName}");
             Destroy(gameObject);
         }
     }
